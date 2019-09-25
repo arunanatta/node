42514a9741f90a5d45afc361854f9ce548de2563
aruna natta <arunanatta@gmail.com>
2019-09-25 23:19:43 +0530

    keywords

---
+++ keywords.js
@@ -0,0 +1,7 @@
+// let -> block scope
+// const -> block scope
+// var -> function scope
+
+//when we assign const to object we can change the key values, we did'nt change the object
+
+// ante dani lopali values in change cheyyochu but object ni change cheyyalem vaatikosam object restrictions untai
