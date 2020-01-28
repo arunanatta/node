3dabe775e18a564c7ec98003f789b8bd8a963349
arunanatta <33624724+arunanatta@users.noreply.github.com>
2020-01-28 23:37:53 +0530

    Create loop-wait.js

---
+++ loop-wait.js
@@ -0,0 +1,15 @@
+loop wait is used for parallel programming, mana response anedi sync lo jaragalante idi use cheyyochu.
+
+const loop = require('loop-wait')
+
+loop.foreach(element=>{
+//code
+
+loop.next();
+})
+
+loop.on(err =>{
+res.send(Err)
+})
+
+res.send(res)
